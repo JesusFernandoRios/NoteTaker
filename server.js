6107c6963ee93744beaa4702db6f51a7908d9222
Jrios <Jesusfernandorios93@gmail.com>
2020-08-01 21:48:40 -0700

    added proper dynamic port before deploying to heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 //Setting up Express
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Use a middleware to parse the JSON data
 app.use(express.urlencoded({ extended: true}));
